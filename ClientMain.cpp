cf50faf2d8a5d299c989459e322d4e15f4ae6988
zhongbin <zhongbin.yang@live.com>
2020-08-27 22:33:51 +0800

    Add files via upload
    test pass with Ubuntu 20.04

---
+++ ClientMain.cpp
@@ -0,0 +1,12 @@
+//
+// Created by zb on 8/27/20.
+//
+
+#include "Client.h"
+// 客户端主函数
+// 创建客户端对象后启动客户端
+int main(int argc, char *argv[]) {
+    Client client;
+    client.Start();
+    return 0;
+}
